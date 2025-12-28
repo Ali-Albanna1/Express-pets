1b61609602fb4272da18892bc487fa6bf42f02dd
Ali-Albanna1 <ali.albanna015@gmail.com>
2025-12-28 15:55:10 +0300

    finish creation of pet

--- controllers/pets.js
+++ controllers/pets.js
@@ -12,7 +12,20 @@ const router = express.Router()
 // POST + /pets/
 
 router.post('/', async (req,res)=>{
- res.json(req.body)
+
+try{
+
+   // use the model to insert data to into db
+   const pet = await Pet.create(req.body)
+   // respond with new pet data doesnt need to create a templates in the case of api use postman 
+   res.status(201).json({pet})
+
+
+}
+catch(error){
+    console.log(error)
+    res.status(500).json({error: 'fail to create pet'})
+}
 })
 
 
